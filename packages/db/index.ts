e9e649288fc016bc941375a01f457aa6182f3513
Dmitrijs Minajevs <dmitrijs.minajevs@outlook.com>
2024-01-09 21:17:41 +0200

    Try fix singleton prisma

--- packages/db/index.ts
+++ packages/db/index.ts
@@ -3,65 +3,63 @@ import { markdownToSafeHTML } from './src/markdown'
 
 export * from '@prisma/client'
 
-const globalForPrisma = globalThis as unknown as {
-  prisma: typeof extendedPrisma
-  prismaWithoutClientExtensions: PrismaClientWithoutExtension
-}
-
 const logQueries = false
 
-const customPrisma = new PrismaClientWithoutExtension({
-  log:
-    process.env.NODE_ENV === 'development'
-      ? [{ emit: 'event', level: 'query' }, 'error', 'warn']
-      : ['error'],
-})
+const prismaSingleton = () => {
+  const customPrisma = new PrismaClientWithoutExtension({
+    log:
+      process.env.NODE_ENV === 'development'
+        ? [{ emit: 'event', level: 'query' }, 'error', 'warn']
+        : ['error'],
+  })
+  customPrisma.$on('query', (e) => {
+    if (!logQueries) return
+    console.log(`[${e.duration}ms] ${e.query}`)
+  })
 
-customPrisma.$on('query', (e) => {
-  if (!logQueries) return
-  console.log(`[${e.duration}ms] ${e.query}`)
-})
-
-const prismaWithoutClientExtensions =
-  globalForPrisma.prismaWithoutClientExtensions ?? customPrisma
-
-const extendedPrisma = prismaWithoutClientExtensions
-  .$extends({
-    result: {
-      collection: {
-        htmlDescription: {
-          needs: { description: true },
-          compute(data) {
-            return data.description !== null
-              ? markdownToSafeHTML(data.description)
-              : null
+  const extendedPrisma = customPrisma
+    .$extends({
+      result: {
+        collection: {
+          htmlDescription: {
+            needs: { description: true },
+            compute(data) {
+              return data.description !== null
+                ? markdownToSafeHTML(data.description)
+                : null
+            },
           },
         },
       },
-    },
-  })
-  .$extends({
-    result: {
-      item: {
-        htmlDescription: {
-          needs: { description: true },
-          compute(data) {
-            return data.description !== null
-              ? markdownToSafeHTML(data.description)
-              : null
+    })
+    .$extends({
+      result: {
+        item: {
+          htmlDescription: {
+            needs: { description: true },
+            compute(data) {
+              return data.description !== null
+                ? markdownToSafeHTML(data.description)
+                : null
+            },
           },
         },
       },
-    },
-  })
+    })
 
-export const prisma = globalForPrisma.prisma ?? extendedPrisma
-
-if (process.env.NODE_ENV !== 'production') {
-  globalForPrisma.prisma = prisma
-  globalForPrisma.prismaWithoutClientExtensions = prismaWithoutClientExtensions
+  return extendedPrisma
 }
+
+declare global {
+  // eslint-disable-next-line no-var
+  var prisma: undefined | ReturnType<typeof prismaSingleton>
+}
+
+export const prisma = globalThis.prisma ?? prismaSingleton()
+
+export default prisma
+
+if (process.env.NODE_ENV !== 'production') globalThis.prisma = prisma
 
 type CustomPrisma = typeof prisma
 export type PrismaClient = CustomPrisma
-export default prisma
