a28384a888cfcfffccda934cb07d80b280e868c1
Dmitrijs Minajevs <dmitrijs.minajevs@outlook.com>
2024-02-12 13:55:34 +0200

    Add QR Code sharing

--- apps/nextjs/src/components/share-dialog/ShareDialog.tsx
+++ apps/nextjs/src/components/share-dialog/ShareDialog.tsx
@@ -1,9 +1,12 @@
-import { Icon } from "@chakra-ui/react"
+import { Box, Icon, IconButton, Tab, TabList, TabPanel, TabPanels, Tabs } from "@chakra-ui/react"
 import { Link } from "@chakra-ui/next-js"
 import { Button, type ButtonProps, Modal, ModalBody, ModalCloseButton, ModalContent, ModalFooter, ModalHeader, ModalOverlay, useDisclosure, Input, InputGroup, InputRightElement, useClipboard } from "@chakra-ui/react"
 import { env } from "~/env.mjs"
-import { CopyCheckIcon, CopyIcon } from "lucide-react"
+import { CopyCheckIcon, CopyIcon, ExternalLinkIcon } from "lucide-react"
 import { cloneElement, type ReactElement } from "react"
+import { QRCodeSVG } from "qrcode.react"
+import EvyLogo from "../../../public/logo.svg"
+
 
 type Props = {
   buttonProps?: Omit<ButtonProps, 'onClick'>
@@ -44,14 +47,40 @@ export const ShareDialog = ({ username, collectionSlug, itemSlug, buttonProps, c
           <ModalHeader>Share</ModalHeader>
           <ModalCloseButton />
           <ModalBody>
-            <InputGroup my='2'>
-              <Input isReadOnly value={url} onFocus={e => e.target.select()} />
-              <InputRightElement width='8rem' justifyContent='end' pr='1.5'>
-                <Button leftIcon={<Icon as={hasCopied ? CopyCheckIcon : CopyIcon} color={hasCopied ? 'green.500' : 'default'} />} h='1.75rem' size='md' onClick={handleCopy} >
-                  {hasCopied ? 'Copied' : 'Copy'}
-                </Button>
-              </InputRightElement>
-            </InputGroup>
+            <Tabs>
+              <TabList>
+                <Tab>Link</Tab>
+                <Tab>QR Code</Tab>
+              </TabList>
+              <TabPanels>
+                <TabPanel p={0}>
+                  <InputGroup my='2'>
+                    <Input isReadOnly value={url} onFocus={e => e.target.select()} />
+                    <InputRightElement width='8rem' justifyContent='end' pr='1.5'>
+                      <IconButton aria-label="open sharing link" icon={<Icon as={ExternalLinkIcon} />} h='1.75rem' mr={1} as={Link} href={url} target="_blank" rel="noopener noreferrer" />
+                      <Button leftIcon={<Icon as={hasCopied ? CopyCheckIcon : CopyIcon} color={hasCopied ? 'green.500' : 'default'} />} h='1.75rem' size='md' onClick={handleCopy} >
+                        {hasCopied ? 'Copied' : 'Copy'}
+                      </Button>
+                    </InputRightElement>
+                  </InputGroup>
+                </TabPanel>
+                <TabPanel>
+                  <Box display='flex' justifyContent='center'>
+                    <QRCodeSVG
+                      size={256}
+                      value={url}
+                      level='M'
+                      imageSettings={{
+                        src: EvyLogo.src,
+                        excavate: true,
+                        height: 64,
+                        width: 64
+                      }}
+                    />
+                  </Box>
+                </TabPanel>
+              </TabPanels>
+            </Tabs>
             {
               isProfilePath
                 ? <Button variant='link' as={Link} href={`/profile/edit`}>Edit username in settings</Button>
